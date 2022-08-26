f0b098a9dff88cd5c9b5c2c284c624fd300a69bd
Gunsmoke017 <Percivalrp106@gmail.com>
2022-08-26 12:11:57 +0100

    simple algorithm to get the period between two dates

--- src/datetimeclass/DateTimeLClass.java
+++ src/datetimeclass/DateTimeLClass.java
@@ -3,11 +3,13 @@ package datetimeclass;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeLClass {
     public static void main(String[] args) {
         LocalDate date;
+        LocalDate date2;
         LocalTime time;
         LocalDateTime dateTime;
 
@@ -20,26 +22,34 @@ public class DateTimeLClass {
         dateTimeFormatter5 = DateTimeFormatter.ofPattern("EEEE,MMMM dd yyyy HH");
 
 
+
         date = LocalDate.now();
         time = LocalTime.now();
         dateTime = LocalDateTime.now();
+        date2 = LocalDate.of(2023,5,29);
+        Period until = date.until(date2);
+        System.out.println("");
+        System.out.println(until.getChronology());
+        System.out.println("Buhari has " + until.getYears() +" years, "+ until.getMonths() + " months and "+ until.getDays()+  " days until he don kpafuka");
 
-        System.out.println("Date : " + date);
-        System.out.println("Time : " + time);
-        System.out.println("Date and Time : " +dateTime);
+        System.out.println(date.minusDays(2).getDayOfWeek());
+//        System.out.println(date.until(date));
+//        System.out.println("Date : " + date);
+//        System.out.println("Time : " + time);
+//        System.out.println("Date and Time : " +dateTime);
 
         System.out.println();
         System.out.println("After formatting the date time is : " + dateTimeFormatter.format(dateTime));
         System.out.println();
         System.out.println("After formatting the date time is format two: " + dateTimeFormatter1.format(dateTime));
         System.out.println();
-        System.out.println("After formatting the date time is format three: " + dateTimeFormatter2.format(dateTime));
+        System.out.println("After formatting the date time is format three : " + dateTimeFormatter2.format(dateTime));
         System.out.println();
-        System.out.println("After formatting the date time is format four: " + dateTimeFormatter3.format(dateTime));
+        System.out.println("After formatting the date time is format four : " + dateTimeFormatter3.format(dateTime));
         System.out.println();
-        System.out.println("After formatting the date time is format five: " + dateTimeFormatter4.format(dateTime));
+        System.out.println("After formatting the date time is format five : " + dateTimeFormatter4.format(dateTime));
         System.out.println();
-        System.out.println("After formatting the date time is format six: " + dateTimeFormatter5.format(dateTime));
+        System.out.println("After formatting the date time is format six : " + dateTimeFormatter5.format(dateTime));
         System.out.println();
 
     }
